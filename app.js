33b2b073769d72dda227b21e59dee37980314900
M-Maira <mmondillo@gmail.com.com>
2025-02-07 00:12:58 -0300

    Creo lista y función agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigo = [];
+
+function agregarAmigo (){
+    let nombreAmigo = document.getElementById("Amigo");
+    let nombreIngresado = nombreAmigo.valeu.trim();
+    
+    // Validar entrada
+
+    if (nombreIngresado === " "){
+        alert ("¡Por favor, ingrese un nombre!");
+    } else {
+        listaAmigo.push(nombreIngresado);
+        return; 
+    }
+}
