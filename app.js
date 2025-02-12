18e154826f2031b04b9a300a6842c3a4d834a600
M-Maira <mmondillo@gmail.com.com>
2025-02-12 00:24:34 -0300

    corrección de funciones

--- app.js
+++ app.js
@@ -3,21 +3,29 @@
 let listaAmigo = [];
 
 function agregarAmigo (){
-    let nombreAmigo = document.getElementById("Amigo");
-    let nombreIngresado = nombreAmigo.valeu.trim();
-    
+    let nombreAmigo = document.getElementById("amigo");
+    let nombreIngresado = nombreAmigo.value.trim();
+    console.log(nombreIngresado)
     // Validar entrada
 
-    if (nombreIngresado === " "){
-        alert ("¡Por favor, ingrese un nombre!");
+    if (nombreIngresado == ""){
+        asignarTextoElemento ("h2", "¡Por favor, ingrese un nombre!");
     } else {
         listaAmigo.push(nombreIngresado);
-        return; 
+        return listaAmigo; 
+        
     }
+    console.log(listaAmigo) 
 }
-
+console.log(listaAmigo)
 // Función limpiar campo de entrada
 
 function limpiarCampo (){
     document.querySelector("#Amigo") = " ";
+}
+
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
