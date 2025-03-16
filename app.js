c0bfa069e4a93446bf3362a5d958ca57b41e4b27
M-Maira <mmondillo@gmail.com.com>
2025-03-15 23:48:34 -0300

    Corrección / crear función actualizarLista

--- app.js
+++ app.js
@@ -4,28 +4,44 @@ let listaAmigo = [];
 
 function agregarAmigo (){
     let nombreAmigo = document.getElementById("amigo");
-    let nombreIngresado = nombreAmigo.value.trim();
-    console.log(nombreIngresado)
+    let nombreIngresado = nombreAmigo.value;
+    console.log(nombreIngresado);
     // Validar entrada
 
-    if (nombreIngresado == ""){
+    if (!nombreIngresado){
         asignarTextoElemento ("h2", "¡Por favor, ingrese un nombre!");
     } else {
         listaAmigo.push(nombreIngresado);
-        return listaAmigo; 
-        
+        limpiarCampo();
+        actualizarLista();
+        return listaAmigo;        
     }
-    console.log(listaAmigo) 
+     
 }
+
 console.log(listaAmigo)
 // Función limpiar campo de entrada
 
 function limpiarCampo (){
-    document.querySelector("#Amigo") = " ";
+    document.querySelector("#amigo").value = "";
+    
 }
 
 function asignarTextoElemento (elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
+}
+
+//Función actualizar lista
+
+function actualizarLista(){
+    let lista= document.getElementById("listaAmigos");
+    lista.innerHTML="";
+    
+    for(let i =0; i< listaAmigo.length; i++){
+        let item = document.createElement("li");
+        item.textContent= listaAmigo[i];
+        lista.appendChild(item);
+    }
 }
